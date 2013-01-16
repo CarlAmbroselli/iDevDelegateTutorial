c9eca5ced416430ac8088854d34bd44dab6cb459
CarlAmbroselli <social@carl-ambroselli.de>
2013-01-16 17:11:38 +0100

    Initial Commit

---
+++ iDeveloperDelegates/iDevAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  iDevAppDelegate.h
+//  iDeveloperDelegates
+//
+//  Created by Carl Ambroselli on 16.01.13.
+//  Copyright (c) 2013 Carl Ambroselli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface iDevAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iDeveloperDelegates/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iDeveloperDelegates
+//
+//  Created by Carl Ambroselli on 16.01.13.
+//  Copyright (c) 2013 Carl Ambroselli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "iDevAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([iDevAppDelegate class]));
+    }
+}
